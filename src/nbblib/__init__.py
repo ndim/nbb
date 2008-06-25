4930a71c429b5b3a77fb73b71abba1329199c283
Hans Ulrich Niedermann <hun@n-dimensional.de>
2008-06-26 01:05:36 +0200

    Import nbblib.newplugins into nbblib namespace

--- src/nbblib/__init__.py
+++ src/nbblib/__init__.py
@@ -4,3 +4,6 @@ from nbblib.package import *
 from nbblib.plugins import *
 from nbblib.vcs import *
 from nbblib.bs import *
+
+for submodule in ('newplugins', ):
+    setattr(__module__, submodule, __import__("nbblib.%s" % submodule))
